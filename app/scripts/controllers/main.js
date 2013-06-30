8e281a9894dd3d2bd38957dc912654ee013c4cfb
Erik van Konijnenburg <ekonijn@xs4all.nl>
2013-06-30 14:00:31 +0200

    move to list page after save

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -6,11 +6,11 @@ angular.module('angularApp')
   });
 
 angular.module('angularApp')
-  .controller('NotesDetailCtl', function ($scope, notesService, $routeParams) {
+  .controller('NotesDetailCtl', function ($scope, notesService, $routeParams, $location) {
     $scope.id = $routeParams.id;
     $scope.data = notesService.get($scope.id);
-
     $scope.save = function() {
       notesService.put($scope.id, $scope.data);
+      $location.path('/notes');
     };
   });
